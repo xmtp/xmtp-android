f68a8cfec8be3a47978237876e25485e3ebab72c
Naomi Plasterer <nplasterer@rewatch.com>
2022-12-20 14:03:03 -0800

    add the library module for the future sdk

--- app/src/androidTest/java/org/xmtp/android/MainActivityInstrumentedTest.kt
+++ app/src/androidTest/java/org/xmtp/android/MainActivityInstrumentedTest.kt
@@ -8,13 +8,8 @@ import org.junit.runner.RunWith
 
 import org.junit.Assert.*
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class MainActivityInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
--- app/src/main/java/org/xmtp/android/MainActivity.kt
+++ app/src/main/java/org/xmtp/android/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "GM $name!")
 }
 
 @Preview(showBackground = true)
