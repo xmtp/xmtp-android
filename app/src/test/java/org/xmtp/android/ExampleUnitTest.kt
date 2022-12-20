317e074cdaec266c5cf74b2089a08ca4bf2707a3
Naomi Plasterer <nplasterer@rewatch.com>
2022-12-20 13:03:53 -0800

    get the xmtp protos imported into the library

--- app/src/test/java/org/xmtp/android/ExampleUnitTest.kt
+++ app/src/test/java/org/xmtp/android/ExampleUnitTest.kt
@@ -1,17 +1,11 @@
 package org.xmtp.android
 
 import org.junit.Test
-
 import org.junit.Assert.*
+import org.xmtp.proto.message.api.v1.MessageApiOuterClass.SortDirection
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    @Test fun testTypesAreAvailable() {
+        assertEquals(1, SortDirection.SORT_DIRECTION_ASCENDING_VALUE)
     }
 }
