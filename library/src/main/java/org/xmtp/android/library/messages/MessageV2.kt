4ae0ee0a322006086a4fd29f79ceeb72e890a48a
Naomi Plasterer <naomi@xmtp.com>
2023-11-03 11:53:50 -0700

    update to use the rust library

--- library/src/main/java/org/xmtp/android/library/messages/MessageV2.kt
+++ library/src/main/java/org/xmtp/android/library/messages/MessageV2.kt
@@ -1,6 +1,5 @@
 package org.xmtp.android.library.messages
 
-import android.util.Log
 import com.google.protobuf.kotlin.toByteString
 import org.web3j.crypto.ECDSASignature
 import org.web3j.crypto.Hash
