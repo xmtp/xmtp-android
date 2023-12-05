3b3cf4955aedccbb5ac38ea3f3a2b4d14214abb1
kele-leanes <ezequiel.leanes@distillery.com>
2023-12-05 17:23:52 -0300

    remove unused import

--- library/src/androidTest/java/org/xmtp/android/library/ClientTest.kt
+++ library/src/androidTest/java/org/xmtp/android/library/ClientTest.kt
@@ -3,7 +3,6 @@ package org.xmtp.android.library
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.assertEquals
 import org.junit.Assert.fail
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.xmtp.android.library.messages.PrivateKeyBuilder
