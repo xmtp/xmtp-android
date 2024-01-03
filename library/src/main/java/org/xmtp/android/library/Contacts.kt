ffa46fa39226dc7006a784da02e6e7043fad993e
Naomi Plasterer <naomi@xmtp.com>
2024-01-03 13:09:39 -0800

    fix consent only fetching first 100 entries

--- library/src/main/java/org/xmtp/android/library/Contacts.kt
+++ library/src/main/java/org/xmtp/android/library/Contacts.kt
@@ -1,5 +1,6 @@
 package org.xmtp.android.library
 
+import android.util.Log
 import kotlinx.coroutines.runBlocking
 import org.xmtp.android.library.messages.ContactBundle
 import org.xmtp.android.library.messages.ContactBundleBuilder
@@ -52,14 +53,13 @@ class ConsentList(val client: Client) {
 
     @OptIn(ExperimentalUnsignedTypes::class)
     suspend fun load(): ConsentList {
-        val envelopes = client.query(
-            Topic.preferenceList(identifier),
+        val envelopes = client.apiClient.envelopes(
+            Topic.preferenceList(identifier).description,
             Pagination(direction = MessageApiOuterClass.SortDirection.SORT_DIRECTION_ASCENDING)
         )
         val consentList = ConsentList(client)
         val preferences: MutableList<PrivatePreferencesAction> = mutableListOf()
-
-        for (envelope in envelopes.envelopesList) {
+        for (envelope in envelopes) {
             val payload = uniffi.xmtp_dh.userPreferencesDecrypt(
                 publicKey.toByteArray().toUByteArray().toList(),
                 privateKey.toByteArray().toUByteArray().toList(),
