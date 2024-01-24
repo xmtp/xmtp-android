bfd9e7bc1c10b5587028ee83318053c9ea2e0d94
Naomi Plasterer <naomi@xmtp.com>
2024-01-24 10:22:21 -0800

    send encoded content

--- library/src/main/java/org/xmtp/android/library/Group.kt
+++ library/src/main/java/org/xmtp/android/library/Group.kt
@@ -1,7 +1,13 @@
 package org.xmtp.android.library
 
 import kotlinx.coroutines.runBlocking
+import org.xmtp.android.library.codecs.ContentCodec
+import org.xmtp.android.library.codecs.EncodedContent
+import org.xmtp.android.library.codecs.compress
 import org.xmtp.android.library.libxmtp.Message
+import org.xmtp.android.library.messages.EnvelopeBuilder
+import org.xmtp.android.library.messages.MessageBuilder
+import org.xmtp.android.library.messages.MessageV2Builder
 import uniffi.xmtpv3.FfiGroup
 import uniffi.xmtpv3.FfiListMessagesOptions
 import java.util.Date
@@ -16,10 +22,36 @@ class Group(val client: Client, private val libXMTPGroup: FfiGroup) {
     fun send(text: String): String {
         runBlocking {
             libXMTPGroup.send(
-                contentBytes = text.toByteArray(Charsets.UTF_8)
+                contentBytes = prepareMessage(content = text, options = null).toByteArray()
             )
         }
         return id.toString()
+    }
+
+    fun <T> prepareMessage(content: T, options: SendOptions?): EncodedContent {
+        val codec = Client.codecRegistry.find(options?.contentType)
+
+        fun <Codec : ContentCodec<T>> encode(codec: Codec, content: Any?): EncodedContent {
+            val contentType = content as? T
+            if (contentType != null) {
+                return codec.encode(contentType)
+            } else {
+                throw XMTPException("Codec type is not registered")
+            }
+        }
+
+        var encoded = encode(codec = codec as ContentCodec<T>, content = content)
+        val fallback = codec.fallback(content)
+        if (!fallback.isNullOrBlank()) {
+            encoded = encoded.toBuilder().also {
+                it.fallback = fallback
+            }.build()
+        }
+        val compression = options?.compression
+        if (compression != null) {
+            encoded = encoded.compress(compression)
+        }
+        return encoded
     }
 
     suspend fun sync() {
