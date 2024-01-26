185231f3c1da6a9a3a1daec9b1cc019022c2bb9f
Naomi Plasterer <naomi@xmtp.com>
2024-01-26 11:23:30 -0800

    make the logger more descriptive

--- library/src/main/java/org/xmtp/android/library/libxmtp/XMTPLogger.kt
+++ library/src/main/java/org/xmtp/android/library/libxmtp/XMTPLogger.kt
@@ -5,6 +5,13 @@ import uniffi.xmtpv3.FfiLogger
 
 class XMTPLogger : FfiLogger {
     override fun log(level: UInt, levelLabel: String, message: String) {
-        Log.i("$level $levelLabel", message)
+        when (level.toInt()) {
+            1 -> Log.e(levelLabel, message)
+            2 -> Log.w(levelLabel, message)
+            3 -> Log.i(levelLabel, message)
+            4 -> Log.d(levelLabel, message)
+            5 -> Log.v(levelLabel, message)
+            else -> Log.i("$level $levelLabel", message)
+        }
     }
 }
