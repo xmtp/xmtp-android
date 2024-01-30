2c7ac43cfa547db7239fcb6810a7156a61a102a4
Naomi Plasterer <naomi@xmtp.com>
2024-01-29 19:01:12 -0800

    write tests for it

---
+++ library/src/androidTest/java/org/xmtp/android/library/GroupMembershipChangeTest.kt
@@ -0,0 +1,84 @@
+package org.xmtp.android.library
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.xmtp.android.library.messages.PrivateKey
+import org.xmtp.android.library.messages.PrivateKeyBuilder
+import org.xmtp.android.library.messages.walletAddress
+import uniffi.xmtpv3.org.xmtp.android.library.codecs.GroupMembershipChangeCodec
+import uniffi.xmtpv3.org.xmtp.android.library.codecs.GroupMembershipChanges
+
+@RunWith(AndroidJUnit4::class)
+class GroupMembershipChangeTest {
+    lateinit var fakeApiClient: FakeApiClient
+    lateinit var alixWallet: PrivateKeyBuilder
+    lateinit var boWallet: PrivateKeyBuilder
+    lateinit var alix: PrivateKey
+    lateinit var alixClient: Client
+    lateinit var bo: PrivateKey
+    lateinit var boClient: Client
+    lateinit var caroWallet: PrivateKeyBuilder
+    lateinit var caro: PrivateKey
+    lateinit var caroClient: Client
+    lateinit var fixtures: Fixtures
+
+    @Before
+    fun setUp() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        fixtures =
+            fixtures(
+                clientOptions = ClientOptions(
+                    ClientOptions.Api(XMTPEnvironment.LOCAL, false),
+                    enableAlphaMls = true,
+                    appContext = context
+                )
+            )
+        alixWallet = fixtures.aliceAccount
+        alix = fixtures.alice
+        boWallet = fixtures.bobAccount
+        bo = fixtures.bob
+        caroWallet = fixtures.caroAccount
+        caro = fixtures.caro
+
+        fakeApiClient = fixtures.fakeApiClient
+        alixClient = fixtures.aliceClient
+        boClient = fixtures.bobClient
+        caroClient = fixtures.caroClient
+    }
+
+    @Test
+    fun testCanAddMembers() {
+        Client.register(codec = GroupMembershipChangeCodec())
+
+        val group = alixClient.conversations.newGroup(listOf(bo.walletAddress, caro.walletAddress))
+        val messages = group.messages()
+        assertEquals(messages.size, 1)
+        val content: GroupMembershipChanges? = messages.first().content()
+        assertEquals(
+            listOf(bo.walletAddress, caro.walletAddress),
+            content?.membersAdded?.map { it.accountAddress }?.sorted()
+        )
+        assert(content?.membersRemoved.isNullOrEmpty())
+    }
+
+    @Test
+    fun testCanRemoveMembers() {
+        Client.register(codec = GroupMembershipChangeCodec())
+
+        val group = alixClient.conversations.newGroup(listOf(bo.walletAddress, caro.walletAddress))
+        val messages = group.messages()
+        assertEquals(messages.size, 1)
+        group.removeMembers(listOf(caro.walletAddress))
+        assertEquals(messages.size, 2)
+        val content: GroupMembershipChanges? = messages.first().content()
+        assertEquals(
+            listOf(caro.walletAddress),
+            content?.membersRemoved?.map { it.accountAddress }?.sorted()
+        )
+        assert(content?.membersAdded.isNullOrEmpty())
+    }
+}
