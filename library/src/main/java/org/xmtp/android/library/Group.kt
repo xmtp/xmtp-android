f5af056628eb7b677b2707998efa36a0617b1443
Naomi Plasterer <naomi@xmtp.com>
2024-03-28 11:51:07 -0700

    change all these places to the topic

--- library/src/main/java/org/xmtp/android/library/Group.kt
+++ library/src/main/java/org/xmtp/android/library/Group.kt
@@ -9,6 +9,7 @@ import org.xmtp.android.library.codecs.compress
 import org.xmtp.android.library.libxmtp.Message
 import org.xmtp.android.library.messages.DecryptedMessage
 import org.xmtp.android.library.messages.PagingInfoSortDirection
+import org.xmtp.android.library.messages.Topic
 import org.xmtp.proto.message.api.v1.MessageApiOuterClass
 import uniffi.xmtpv3.FfiGroup
 import uniffi.xmtpv3.FfiGroupMetadata
@@ -23,6 +24,9 @@ import kotlin.time.DurationUnit
 class Group(val client: Client, private val libXMTPGroup: FfiGroup) {
     val id: ByteArray
         get() = libXMTPGroup.id()
+
+    val topic: String
+        get() = Topic.groupMessage(id.toHex()).description
 
     val createdAt: Date
         get() = Date(libXMTPGroup.createdAtNs() / 1_000_000)
