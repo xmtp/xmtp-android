778391dd60b060d1aa3d5ec6b21d1b67a6388f23
Naomi Plasterer <naomi@xmtp.com>
2024-05-12 11:22:28 -0700

    fix up the linter issue

--- library/src/androidTest/java/org/xmtp/android/library/ConversationsTest.kt
+++ library/src/androidTest/java/org/xmtp/android/library/ConversationsTest.kt
@@ -9,7 +9,6 @@ import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertTrue
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.xmtp.android.library.codecs.TextCodec
