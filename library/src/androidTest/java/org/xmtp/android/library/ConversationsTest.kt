ed5f828275bfaa57a663b505ca5ad101985885fd
Naomi Plasterer <naomi@xmtp.com>
2024-12-18 21:27:55 -0800

    fix up the linter issues

--- library/src/androidTest/java/org/xmtp/android/library/ConversationsTest.kt
+++ library/src/androidTest/java/org/xmtp/android/library/ConversationsTest.kt
@@ -1,7 +1,6 @@
 package org.xmtp.android.library
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -13,7 +12,6 @@ import org.junit.runner.RunWith
 import org.xmtp.android.library.messages.PrivateKey
 import org.xmtp.android.library.messages.PrivateKeyBuilder
 import org.xmtp.android.library.messages.walletAddress
-import java.security.SecureRandom
 
 @RunWith(AndroidJUnit4::class)
 class ConversationsTest {
