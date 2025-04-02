a18a0413882710353bf539869deeec5c033aadf9
cameronvoell <cameronvoell@users.noreply.github.com>
2025-04-01 19:28:23 -0700

    verify key packages

--- library/src/main/java/org/xmtp/android/library/Client.kt
+++ library/src/main/java/org/xmtp/android/library/Client.kt
@@ -8,8 +8,10 @@ import org.xmtp.android.library.codecs.ContentCodec
 import org.xmtp.android.library.codecs.TextCodec
 import org.xmtp.android.library.libxmtp.IdentityKind
 import org.xmtp.android.library.libxmtp.InboxState
+import org.xmtp.android.library.libxmtp.Installation
 import org.xmtp.android.library.libxmtp.PublicIdentity
 import org.xmtp.android.library.libxmtp.SignatureRequest
+import uniffi.xmtpv3.FfiKeyPackageStatus
 import uniffi.xmtpv3.FfiXmtpClient
 import uniffi.xmtpv3.XmtpApiClient
 import uniffi.xmtpv3.connectToBackend
@@ -130,6 +132,19 @@ class Client(
             return withFfiClient(api) { ffiClient ->
                 ffiClient.addressesFromInboxId(true, inboxIds)
                     .map { InboxState(it) }
+            }
+        }
+
+        suspend fun keyPackageStatusesForInstallationIds(
+            installationIds: List<String>,
+            api: ClientOptions.Api,
+        ): Map<String, FfiKeyPackageStatus> {
+            return withFfiClient(api) { ffiClient ->
+                val byteArrays = installationIds.map { it.hexToByteArray() }
+                val result = ffiClient.getKeyPackageStatusesForInstallationIds(byteArrays)
+                result.entries.associate { (byteArrayKey, status) ->
+                    byteArrayKey.toHex() to status
+                }
             }
         }
 
