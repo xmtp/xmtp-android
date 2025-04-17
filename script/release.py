1a3226cb94d708c4ca16c7de7a995c1cc9ff5d7e
cameronvoell <cameronvoell@users.noreply.github.com>
2025-04-17 12:50:07 -0700

    update release script/action for 4.0.4 release

--- script/release.py
+++ script/release.py
@@ -2,15 +2,15 @@
 import os
 import subprocess
 
-if (input("\nAre you sure? This will replace the current \"release\" branch with origin/main. Y/n\n") != "Y"):
+if (input("\nAre you sure? This will replace the current \"release\" branch with origin/release-hotfix/4.0. Y/n\n") != "Y"):
   print(u'\U0001f44C' + " lmk if you change your mind")
   exit(0)
 
 os.system("git fetch origin")
 os.system("git checkout release")
 
-os.system("git reset --hard origin/main")
+os.system("git reset --hard origin/release-hotfix/4.0")
 os.system("git push --force origin release")
 
-print(u'\U0001f680' + " Successfully pushed origin/main to release")
+print(u'\U0001f680' + " Successfully pushed origin/release-hotfix/4.0 to release")
 print(u'\U0001f440' + " Follow the deploy at https://github.com/xmtp/xmtp-android/actions")
