961c715a6a81dfba8ce0c015a56f858146c4652a
Naomi Plasterer <naomi@xmtp.com>
2025-05-07 14:58:37 -0700

    fix up the test

--- library/src/androidTest/java/org/xmtp/android/library/GroupTest.kt
+++ library/src/androidTest/java/org/xmtp/android/library/GroupTest.kt
@@ -1028,7 +1028,7 @@ class GroupTest {
 
         assertEquals(runBlocking { alixGroup.messages() }.size, 1)
         assertEquals(runBlocking { alixGroup2.messages() }.size, 1)
-        assertEquals(numGroups, 2u) // I still think this should be 3
+        assertEquals(numGroups, 2u)
 
         runBlocking {
             boGroup2.removeMembers(listOf(alixClient.inboxId))
@@ -1043,13 +1043,13 @@ class GroupTest {
         assertEquals(runBlocking { alixGroup.messages() }.size, 3)
         assertEquals(runBlocking { alixGroup2.messages() }.size, 2)
         // First syncAllGroups after remove includes the group you're removed from
-        assertEquals(numGroups, 3u)
+        assertEquals(numGroups, 2u)
 
         runBlocking {
             numGroups = alixClient.conversations.syncAllConversations()
         }
         // Next syncAllGroups will not include the inactive group
-        assertEquals(numGroups, 2u)
+        assertEquals(numGroups, 1u)
     }
 
     @Test
