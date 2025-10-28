d0daabab55f494998a3a9d1af3d8e1336f595493
cameronvoell <cameronvoell@users.noreply.github.com>
2025-10-28 11:31:57 -0700

    temporarily disable api cache while libxmtp logic is being updated

--- library/src/main/java/org/xmtp/android/library/Client.kt
+++ library/src/main/java/org/xmtp/android/library/Client.kt
@@ -122,10 +122,10 @@ class Client(
         private fun ClientOptions.Api.toCacheKey(): String =
             "${env.getUrl()}|$isSecure|${appVersion ?: "nil"}|${gatewayHost ?: "nil"}"
 
-        private val apiClientCache = mutableMapOf<String, XmtpApiClient>()
-        private val cacheLock = Mutex()
-        private val syncApiClientCache = mutableMapOf<String, XmtpApiClient>()
-        private val syncCacheLock = Mutex()
+//       private val apiClientCache = mutableMapOf<String, XmtpApiClient>()
+       private val cacheLock = Mutex()
+//       private val syncApiClientCache = mutableMapOf<String, XmtpApiClient>()
+       private val syncCacheLock = Mutex()
 
         fun activatePersistentLibXMTPLogWriter(
             appContext: Context,
@@ -185,33 +185,33 @@ class Client(
         }
 
         suspend fun connectToApiBackend(api: ClientOptions.Api): XmtpApiClient {
-            val cacheKey = api.toCacheKey()
+//            val cacheKey = api.toCacheKey()
             return cacheLock.withLock {
-                val cached = apiClientCache[cacheKey]
-
-                if (cached != null && isConnected(cached)) {
-                    return cached
-                }
+//               val cached = apiClientCache[cacheKey]
+//
+//               if (cached != null && isConnected(cached)) {
+//                   return cached
+//               }
 
                 // If not cached or not connected, create a fresh client
                 val newClient = connectToBackend(api.env.getUrl(), api.gatewayHost, api.isSecure, api.appVersion)
-                apiClientCache[cacheKey] = newClient
+//               apiClientCache[cacheKey] = newClient
                 return@withLock newClient
             }
         }
 
         suspend fun connectToSyncApiBackend(api: ClientOptions.Api): XmtpApiClient {
-            val cacheKey = api.toCacheKey()
+//            val cacheKey = api.toCacheKey()
             return syncCacheLock.withLock {
-                val cached = syncApiClientCache[cacheKey]
-
-                if (cached != null && isConnected(cached)) {
-                    return cached
-                }
+////                val cached = syncApiClientCache[cacheKey]
+//
+//                if (cached != null && isConnected(cached)) {
+//                    return cached
+//                }
 
                 // If not cached or not connected, create a fresh client
                 val newClient = connectToBackend(api.env.getUrl(), api.gatewayHost, api.isSecure, api.appVersion)
-                syncApiClientCache[cacheKey] = newClient
+//                syncApiClientCache[cacheKey] = newClient
                 return@withLock newClient
             }
         }
